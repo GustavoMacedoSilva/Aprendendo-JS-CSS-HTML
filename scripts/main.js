dce1d175a6b25ebfe248b290008ee99b35dae212
Gustavo M Silva <silvamacedo.gustavo2004@gmail.com>
2025-04-09 17:49:37 -0300

    correção

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,12 @@ function setUsername(){
     localStorage.setItem("name", myName);
     myHeading.textContent = "Welcome, " + myName;
 }
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
@@ -12,10 +18,13 @@ myImage.addEventListener("click", () => {
   if (mySrc === "images/fred-durst.jpg") {
     myImage.setAttribute("src", "images/slipknot_integrants.webp");
     if (myDesc === "descricao"){
-        descricao.textContent = "This is a picture of the band Slipknot, an American heavy metal band formed in 1995 in Des Moines, Iowa. The band is known for its aggressive style and energetic live performances, as well as its distinctive image, which includes masks and uniforms worn by the members.";
+        descricao.textContent = "Slipknot is a Metal band, they are known for their masks and red jumpsuits.";
     }
   } else {
     myImage.setAttribute("src", "images/fred-durst.jpg");
+    if (myDesc === "descricao"){
+        descricao.textContent = "Limp Bizkit is a Nu Metal band, some of their music mixed Metal with Hip-Hop.";
+    }
   }
 });
 
